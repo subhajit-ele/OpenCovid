22d73697f7de80f9dd881b0fee3df360277634ef
subhajit-ele <subhajit.ele@gmail.com>
2020-05-09 12:17:20 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,1 @@
-"{\"bannerText\":\"Subhajit\"}"
+"{\"bannerText\":\"This is a strange start to the week after a strange weekend where we've seen the empty streets across Rome, Paris, and Madrid, then hearing about lockdowns extending to Los Angeles and New York and governments drastically reducing interest rates around the world.\"}"
